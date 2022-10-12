c0f8e46cafe2616a3b497cd280d7093e62610424
Nirose k <111358060+knirose077@users.noreply.github.com>
2022-10-12 22:44:18 +0530

    Create student.cs

---
+++ 28 sep/data acc/student.cs
@@ -0,0 +1,11 @@
+namespace dataaccess
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class student
+    {
+        public int studentid { get; set; }
+        public string studentname { get; set; }
+    }
+    }
