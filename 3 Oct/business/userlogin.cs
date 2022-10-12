3bc6075ffa2102896f3c5ae69b661ae017b09c65
Nirose k <111358060+knirose077@users.noreply.github.com>
2022-10-12 23:01:43 +0530

    Create userlogin.cs

---
+++ 3 Oct/business/userlogin.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business
+{
+    public class  userlogin
+    {
+        public string name { get; set; }
+        public int userid { get; set; } 
+        public string userpassword { get; set; }
+
+    }
+}
